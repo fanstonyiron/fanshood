0c0ef94caaf3daea3523e40b954bd211a46d4233
FansHood <148871082+fanstonyiron@users.noreply.github.com>
2023-10-25 23:11:29 -0500

    Update FanshoodData.sol

--- FanshoodData.sol
+++ FanshoodData.sol
@@ -4,11 +4,8 @@ pragma solidity ^0.8.19;
 library FanshoodData {
     struct BookOption {
         bool isBook;
-        //预售时间
         uint256 bookTime;
-        //开放购买时间
         uint256 openTime;
-        //merke根
         bytes32 whitelistRoot;
     }
 }
